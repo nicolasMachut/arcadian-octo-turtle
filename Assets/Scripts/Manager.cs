4c4017b2883a596fe24c4f108a09115e23e0f308
unknown <johan.henin@gmail.com>
2015-09-22 15:50:54 +0200

    tentative correction

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -8,7 +8,6 @@ public class Manager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         print("instanciate");
-        PhotonNetwork.isMessageQueueRunning = false;
         PhotonNetwork.Instantiate("Joueur", Vector3.zero, Quaternion.identity, 0);
     }
 	
