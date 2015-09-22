a26cf78db7ef59066e054710939ddd95f25f96f5
nicolasMachut <machut.nicolas@gmail.com>
2015-09-22 14:27:27 +0200

    modif joueur

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -8,7 +8,7 @@ public class Manager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         print("instanciate");
-        PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity, 0);
+        PhotonNetwork.Instantiate("Joueur", Vector3.zero, Quaternion.identity, 0);
     }
 	
 	// Update is called once per frame
