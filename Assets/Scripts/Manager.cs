7335d304b0829668d02c3c751331c0dd94acccdb
nicolasMachut <machut.nicolas@gmail.com>
2015-09-23 11:28:01 +0200

    test2

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -8,7 +8,7 @@ public class Manager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         print("instanciate");
-        PhotonNetwork.Instantiate("PlayerBoule", new Vector3(-45, 10, 0), Quaternion.identity, 0);
+        PhotonNetwork.Instantiate("PlayerBoule", new Vector3(-47, 10, 0), Quaternion.identity, 0);
     }
 	
 	// Update is called once per frame
