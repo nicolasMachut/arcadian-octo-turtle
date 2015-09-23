131c3b5761832de7197c9d42f9b625daa18901b8
nicolasMachut <machut.nicolas@gmail.com>
2015-09-23 11:35:36 +0200

    test

--- Assets/Scripts/firstScene/PlayerController.cs
+++ Assets/Scripts/firstScene/PlayerController.cs
@@ -11,41 +11,56 @@ public class PlayerController : MonoBehaviour {
 
 	public bool candoublejump;
 	public bool grounded;
+    private PhotonView view;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
-        //view = GetComponent<PhotonView>();
+        view = GetComponent<PhotonView>();
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			if (grounded) {
-				rb.AddForce (new Vector3 (0.0f, jumpForce,0.0f));
-				candoublejump = true;
-				grounded = false;
-			} else {
-				if (candoublejump) {
-					candoublejump = false;
-					rb.AddForce (new Vector3 (0.0f, jumpForce, 0.0f));
-				}
-			}
-		}
+        if (view.isMine)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (grounded)
+                {
+                    rb.AddForce(new Vector3(0.0f, jumpForce, 0.0f));
+                    candoublejump = true;
+                    grounded = false;
+                }
+                else
+                {
+                    if (candoublejump)
+                    {
+                        candoublejump = false;
+                        rb.AddForce(new Vector3(0.0f, jumpForce, 0.0f));
+                    }
+                }
+            }
 
-		if (rb.velocity.magnitude < maxSpeed) {
-			if (Input.GetKey (KeyCode.LeftArrow)) {
-				moveLeft ();
-			}
-			if (Input.GetKey (KeyCode.RightArrow)) {
-				moveRight ();
-			}
-		} else {
-			if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.RightArrow)) {
-				rb.AddForce(-2 * rb.velocity);
-			}
-		}
+            if (rb.velocity.magnitude < maxSpeed)
+            {
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    moveLeft();
+                }
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    moveRight();
+                }
+            }
+            else
+            {
+                if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+                {
+                    rb.AddForce(-2 * rb.velocity);
+                }
+            }
+        }
 	}
 
 	void moveLeft(){
